06e64c20afa8918d8e1e1b01815430759a88eeec
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2022-12-13 16:44:50 +0800

    Category
    - CRUD

--- server/queries/index.js
+++ server/queries/index.js
@@ -4,6 +4,7 @@ const db = require('../connection');
 // Crud
 const Authentication = require('./crud/Authentication');
 const Users = require('./crud/Users');
+const Category = require('./crud/Category');
 
 const authentication = (data, type) => { return new Authentication(data)[type](); }
 
@@ -19,6 +20,7 @@ const list = (table, query) => {
     return new Promise(async resolve => {
         switch(table) {
             case 'tbl_users': resolve(await new Users(query)['list']()); break;
+            case 'tbl_pet_category': resolve(await new Category(query)['list']()); break;
         }
     });
 }
@@ -27,6 +29,7 @@ const get = (table, type, query) => {
     return new Promise(async (resolve) => {
         switch(table) {
             case 'tbl_users': resolve(await new Users(query)[type]()); break;
+            case 'tbl_pet_category': resolve(await new Category(query)[type]()); break;
         }
     });
 }
@@ -35,6 +38,7 @@ const save = (table, type, data) => {
     return new Promise(resolve => {
         switch(table) {
             case 'tbl_users': resolve(new Users('', data)[type]()); break
+            case 'tbl_pet_category': resolve(new Category('', data)[type]()); break;
         }
     });
 }
@@ -43,6 +47,7 @@ const update = (table, type, query, data) => {
     return new Promise(resolve => {
         switch(table) {
             case 'tbl_users': resolve(new Users(query, data)[type]()); break;
+            case 'tbl_pet_category': resolve(new Category(query, data)[type]()); break;
         }
     });
 }
