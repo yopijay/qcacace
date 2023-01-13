da02925282755a537c276a836133eb2ba2215638
Pijay <53463564+yopijay@users.noreply.github.com>
2023-01-13 23:55:32 +0800

    Trial & Error

--- admin/src/pages/account/sign-in/index.jsx
+++ admin/src/pages/account/sign-in/index.jsx
@@ -15,8 +15,8 @@ import { Validation } from "./index.validation"; // Validation
 const Index = () => {
     const { register, formState: { errors }, handleSubmit, setError } = useForm({ resolver: yupResolver(Validation()) });
     const { mutate: signin } = usePost({ fetch: authentication, onSuccess: (data) => {
-        if(data.result === 'error') { (data.error).forEach((err, index) => { setError(err.name, { type: index === 0 ? 'focus' : '', message: err.message }, { shouldFocus: index === 0 }); }); }
-        else { console.log(data); }
+        console.log(data);
+        // if(data.result === 'error') { (data.error).forEach((err, index) => { setError(err.name, { type: index === 0 ? 'focus' : '', message: err.message }, { shouldFocus: index === 0 }); }); }
         // else { localStorage.setItem('token', data.message?.id); window.location.href = '/'; }
     }});
 
--- server/queries/tables/Users.js
+++ server/queries/tables/Users.js
@@ -6,24 +6,25 @@ class Users {
     specific = async (id) => { return (await new Builder(`tbl_users AS usr`).select().join({ table: `tbl_users_info AS info`, condition: `info.user_id = usr.id`, type: 'LEFT' }).condition(`WHERE usr.id= ${id}`).build()).rows; }
 
     login = async (data) => {
-        let email = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}'`).build();
-        let  verified = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}' AND is_email_verified= 1`).build();
-        let creds = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}' AND password= '${data.password}'`).build();
+        return data;
+        // let email = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}'`).build();
+        // let verified = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}' AND is_email_verified= 1`).build();
+        // let creds = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}' AND password= '${data.password}'`).build();
 
-        if(email.rowCount === 1) {
-            if(verified.rowCount === 1) {
-                if(creds.rowCount === 1) {
-                    if(creds.rows[0].is_logged === 0) {
-                        let logged_in = await new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).build();
-                        return { result: 'success', message: { id: btoa(logged_in.rows[0].id) } }
-                    }
-                    else { return { result: 'error', error: [{ name: 'email', message: 'Account already used in another device' }] } }
-                }
-                else { return { result: 'error', error: [{ name: 'password', message: 'Incorrect password!' }] } }
-            }
-            else { return { result: 'error', error: [{ name: 'email', message: 'Email is not yet verified!' }] } }
-        }
-        else { return { result: 'error', error: [{ name: 'email', message: 'Email doesn`t exist!' }] } }
+        // if(email.rowCount === 1) {
+        //     if(verified.rowCount === 1) {
+        //         if(creds.rowCount === 1) {
+        //             if(creds.rows[0].is_logged === 0) {
+        //                 let logged_in = await new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).build();
+        //                 return { result: 'success', message: { id: btoa(logged_in.rows[0].id) } }
+        //             }
+        //             else { return { result: 'error', error: [{ name: 'email', message: 'Account already used in another device' }] } }
+        //         }
+        //         else { return { result: 'error', error: [{ name: 'password', message: 'Incorrect password!' }] } }
+        //     }
+        //     else { return { result: 'error', error: [{ name: 'email', message: 'Email is not yet verified!' }] } }
+        // }
+        // else { return { result: 'error', error: [{ name: 'email', message: 'Email doesn`t exist!' }] } }
     }
 
     logout = async (data) => {
