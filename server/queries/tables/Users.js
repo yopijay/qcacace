87fc470d7d69dda5667472cc3b541b4e81c27a3c
Pijay <53463564+yopijay@users.noreply.github.com>
2023-01-14 00:29:17 +0800

    Update Users.js

--- server/queries/tables/Users.js
+++ server/queries/tables/Users.js
@@ -10,26 +10,21 @@ class Users {
         let verified = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}' AND is_email_verified= 1`).build();
         let creds = await new Builder(`tbl_users`).select().condition(`WHERE email= '${data.email}' AND password= '${data.password}'`).build();
 
-        return {
-            email,
-            verified,
-            creds
+        if(email.rowCount > 0) {
+            if(verified.rowCount > 0) {
+                if(creds.rowCount > 0) {
+                    if(creds.rows[0].is_logged === 0) {
+                        let logged_in = await new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).build();
+                        console.log(logged_in);
+                        // return { result: 'success', message: { id: btoa(logged_in.rows[0].id) } }
+                    }
+                    else { return { result: 'error', error: [{ name: 'email', message: 'Account already used in another device' }] } }
+                }
+                else { return { result: 'error', error: [{ name: 'password', message: 'Incorrect password!' }] } }
+            }
+            else { return { result: 'error', error: [{ name: 'email', message: 'Email is not yet verified!' }] } }
         }
-
-        // if(email.rowCount === 1) {
-        //     if(verified.rowCount === 1) {
-        //         if(creds.rowCount === 1) {
-        //             if(creds.rows[0].is_logged === 0) {
-        //                 let logged_in = await new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).build();
-        //                 return { result: 'success', message: { id: btoa(logged_in.rows[0].id) } }
-        //             }
-        //             else { return { result: 'error', error: [{ name: 'email', message: 'Account already used in another device' }] } }
-        //         }
-        //         else { return { result: 'error', error: [{ name: 'password', message: 'Incorrect password!' }] } }
-        //     }
-        //     else { return { result: 'error', error: [{ name: 'email', message: 'Email is not yet verified!' }] } }
-        // }
-        // else { return { result: 'error', error: [{ name: 'email', message: 'Email doesn`t exist!' }] } }
+        else { return { result: 'error', error: [{ name: 'email', message: 'Email doesn`t exist!' }] } }
     }
 
     logout = async (data) => {
