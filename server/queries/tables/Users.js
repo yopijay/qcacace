d6507321e13cc086c1c0ee68a2bb96f46cd423bc
Pijay <53463564+yopijay@users.noreply.github.com>
2023-01-14 00:38:04 +0800

    Update Users.js

--- server/queries/tables/Users.js
+++ server/queries/tables/Users.js
@@ -14,7 +14,7 @@ class Users {
             if(verified.rowCount > 0) {
                 if(creds.rowCount > 0) {
                     if(creds.rows[0].is_logged === 0) {
-                        let logged_in = await new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).build();
+                        let logged_in = new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).test();
                         console.log(logged_in);
                         // return { result: 'success', message: { id: btoa(logged_in.rows[0].id) } }
                     }
