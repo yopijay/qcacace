33c73e33e69403a96f70faa87a12c4254d9cb459
Pijay <53463564+yopijay@users.noreply.github.com>
2023-01-14 01:31:04 +0800

    Sign in

--- server/queries/tables/Users.js
+++ server/queries/tables/Users.js
@@ -15,7 +15,7 @@ class Users {
                 if(creds.rowCount > 0) {
                     if(creds.rows[0].is_logged === 0) {
                         let logged_in = await new Builder(`tbl_users`).update(`is_logged= 1`).condition(`WHERE id= ${creds.rows[0].id} RETURNING id`).build();
-                        return { result: 'success', id: btoa(logged_in.rows[0].id) }
+                        return { result: 'success', id: logged_in.rows[0].id }
                     }
                     else { return { result: 'error', error: [{ name: 'email', message: 'Account already used in another device' }] } }
                 }
