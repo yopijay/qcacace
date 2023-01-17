b0a9c0382954b8f65ee2715fa35d05acf506de14
Pijay <53463564+yopijay@users.noreply.github.com>
2023-01-17 19:29:02 +0800

    Sidebar
    - Footer

--- client/src/pages/global/sidebar/layouts/Footer.jsx
+++ client/src/pages/global/sidebar/layouts/Footer.jsx
@@ -8,10 +8,8 @@ import Logo from 'assets/images/logo.png';
 
 // Custom styles
 export const brand = {
-    fontFamily: 'Tommy Bold',
     letterSpacing: 1,
-    fontSize: { md: '115%' },
-    color: '#ffffff'
+    color: '#919eab'
 }
 
 export const brandinfo = {
@@ -28,17 +26,10 @@ export const brandlinks = {
 
 const Footer = () => {
     return (
-        <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" sx= {{ padding: { xs: '20px', lg: '90px 0 20px 0' }, backgroundColor: '#1B4168' }}>
-            <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" spacing= { 2 }>
-                <Avatar src= { Logo } alt= "logo" sx= {{ width: 40, height: 40 }} />
-                <Typography sx= { brand}>QCACAC E - Services</Typography>
-            </Stack>
-            <Typography sx= { brandinfo }>The Quezon City Animal Care and Adoption Center E-Services provides services that makes adoption, surrendering, and reporting missing animals easier. 
-            It also provides information about Quezon City Animals including latest announcements to keep you updated. 
-            Here in QC ACAC, we offer love and care to the animals!.</Typography>
-            <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" spacing= { 1 }>
-                <Typography sx= { brandlinks }><FontAwesomeIcon icon= { brands('facebook') } /></Typography>
-                <Typography sx= { brandlinks }><FontAwesomeIcon icon= { brands('facebook-messenger') } /></Typography>
+        <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" sx= {{ padding: { xs: '10px', lg: '90px 0 20px 0' } }}>
+            <Stack direction= "row" justifyContent= "center" alignItems= "center" spacing= { 2 }>
+                <Avatar src= { Logo } alt= "logo" sx= {{ width: 30, height: 30 }} />
+                <Typography sx= { brand }>QCACAC E - Services</Typography>
             </Stack>
         </Stack>
     );
