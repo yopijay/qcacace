1b66e5fcd948e96c115cfabe95ed14e0f2562bdf
Pijay <53463564+yopijay@users.noreply.github.com>
2023-02-07 18:16:26 +0800

    Credentials

--- server/functions/global.js
+++ server/functions/global.js
@@ -1,8 +1,8 @@
 // Global
 const Builder = require("./builder");
 
-const USER = "flipmusicc@gmail.com";
-const PASS = "tujekagryhvrkuok";
+const USER = "qcacac2017@gmail.com";
+const PASS = "jrcxdiysopxohcve";
 const checkifsame = (value1, value2) => { return ((value1 !== null && value1 !== '' && value1 !== undefined ? value1 : null) !== (value2 !== null && value2 !== '' && value2 !== undefined ? value2 : null)); }
 const series = (label, count, limit = 7) => { return `${label}${('0000000' + count).substr(('0000000' + count).length - limit)}`; }
 
