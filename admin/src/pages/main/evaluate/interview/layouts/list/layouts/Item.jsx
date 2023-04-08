289e2ab65a0baac91961629788cd890368631578
Pijay <53463564+yopijay@users.noreply.github.com>
2023-04-08 18:47:56 +0800

    Update Item.jsx

--- admin/src/pages/main/evaluate/interview/layouts/list/layouts/Item.jsx
+++ admin/src/pages/main/evaluate/interview/layouts/list/layouts/Item.jsx
@@ -34,7 +34,7 @@ const Item = () => {
                 <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" key= { index } sx= { item }>
                     <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" sx= {{ flexGrow: 1 }}>
                         <Stack direction= "column" justifyContent= "flex-start" alignItems= "flex-start">
-                            { console.lo(data) }
+                            { console.log(data) }
                             {/* <Typography variant= "body1" 
                                 sx= {{ fontWeight: 'bold', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>#{ data.series_no }</Typography>
                             <Typography variant= "body2" 
